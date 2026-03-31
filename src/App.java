845d3de6a4d8beb3b810bc5fd2f8a185c8f5611c
Nick <usr4227@salesianos-lacuesta.net>
2026-03-31 13:13:16 +0100

    Viajero

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+
+import net.salesianos.personajes.Guerrero;
+import net.salesianos.personajes.Mago;
+import net.salesianos.personajes.Personaje;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        Personaje viajero = new Personaje("Eldrin", 3);
+        viajero.entrenar(4);
+        viajero.gastarEnergia(30);
+        viajero.mostrarInfo();
+        Personaje.mostrarPersonajes();
+
     }
 }
