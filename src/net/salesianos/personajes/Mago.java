0af7effb5674900fcadcac00852d449cbd2b0edd
Nick <usr4227@salesianos-lacuesta.net>
2026-03-31 14:11:08 +0100

    Getters y setters

--- src/net/salesianos/personajes/Mago.java
+++ src/net/salesianos/personajes/Mago.java
@@ -21,5 +21,28 @@ public class Mago extends Personaje {
             energia = 0;
         }
     }
+ public String getNombre() {
+        return nombre;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public int getEnergia() {
+        return energia;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public void setEnergia(int energia) {
+        this.energia = energia;
+    }
 
 }
